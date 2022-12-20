664e39dd1a06b2f8e350b8e8faebbd5ed670edf1
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-12-20 18:46:36 +0530

    Banker's Algo for Deadlock Avoidation

---
+++ Banker's Algo.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+int main(){
+    int n,res;
+    printf("Enter the number of programs\n");
+    scanf("%d",&n);
+    printf("Enter the number of resources\n");
+    scanf("%d",&res);
+    int allocated[n][res];
+    int rem[res];
+    memset(rem,0,sizeof(rem));
+    
+    printf("Enter available entities of resources\n");
+    for(int i=0;i<res;i++)scanf("%d",&rem[i]);
+    
+    printf("Enter the allocated resources for programs\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<res;j++){
+            scanf("%d",&allocated[i][j]);
+            rem[j]-=allocated[i][j];
+        }
+    }
+    int needed[n][res];
+    printf("Enter the needed resources for programs\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<res;j++){
+            scanf("%d",&needed[i][j]);
+            needed[i][j]-=allocated[i][j];
+        }
+    }
+    int done[n];
+    memset(done,0,sizeof(done));
+    while(1){
+        int flag=0;
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            if(done[i])continue;
+            flag=1;
+            for(int j=0;j<res;j++){
+                if(rem[j]>=needed[i][j]){
+                    cnt++;
+                }
+            }
+            if(cnt==res){
+                printf("program-%d-->",i+1);
+                for(int j=0;j<res;j++){
+                    rem[j]+=needed[i][j];
+                    needed[i][j]=0;
+                }
+                done[i]=1;
+            }
+        }
+        if(!flag)break;
+    }
+}
