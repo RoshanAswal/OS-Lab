b5d1d380eafc522372e16b058eaec432326572d1
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-12-26 15:55:00 +0530

    Update SumUsingFork.c

--- SumUsingFork.c
+++ SumUsingFork.c
@@ -5,14 +5,14 @@ int main()
     int arr[]={1,2,3,4,5};
     int id=fork();
     int sumEven=0,sumOdd=0;
-    if(id>0) // a child process
+    if(id==0) // a child process
     {
         for(int i=0;i<5;i++){
             if(arr[i]&1)sumOdd+=arr[i];
         }
         printf("Parent process\n");
         printf("Odd sum - %d\n",sumOdd);
-    }else{
+    }else if(id>0){
         for(int i=0;i<5;i++){
             if(!(arr[i]&1))sumEven+=arr[i];
         }
